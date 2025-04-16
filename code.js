b9acacb68b2cb48b07e603e24664011dd6a75ade
Aidan Newberry <91752566+regtoga@users.noreply.github.com>
2025-04-16 11:27:00 -0600

    darn, i used memoization... 1 sec ill redo it.

--- code.js
+++ code.js
@@ -1,9 +1,16 @@
+cache = [];
+
 function factorial(n) {
     if(n === 0) return 1;
-    else return n * factorial(n - 1);
-}
+    else{
+        if (cache[n] == null){
+            cache[n] = n * factorial(n - 1);
+        }
+        return cache[n];
+    }
+}   
 
-function e(n) {
+function ee(n) {
     if(n === 0) return 1;
-    else return 1.0 / factorial(n) + e(n - 1);
+    else return 1.0 / factorial(n) + ee(n - 1);
 }
